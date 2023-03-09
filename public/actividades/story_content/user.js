b0315022a8e0e60244aff3260715ffd6be03762e
jorozcop <orojacobo@gmail.com>
2023-03-09 16:51:17 -0500

    vinculación AD

---
+++ public/actividades/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6bD1rf6oG4y":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.close();
+}
+
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -24,7 +24,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Verdadero y falso
             .col-auto
-              a.boton.boton--b(style="background-color:#FECB2F" :href="obtenerLink('/actividades/actividad1/index.html')" target="_blank")
+              a.boton.boton--b(style="background-color:#FECB2F" :href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
